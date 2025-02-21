bfece81ed89946de13f96c08f4ca135fd7f99df1
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-02-21 09:48:06 +0100

    initial commit

---
+++ services/job-parser-service/src/main/java/com/moldavets/microservices/job_parser_service/JobParserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moldavets.microservices.job_parser_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobParserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobParserServiceApplication.class, args);
+	}
+
+}
---
+++ services/job-parser-service/src/test/java/com/moldavets/microservices/job_parser_service/JobParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldavets.microservices.job_parser_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobParserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
